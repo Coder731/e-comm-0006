be626e3b9e8f548a4afc7ec4502c91785f861a1e
Coder731 <stephenobrien1@gmail.com>
2022-06-30 17:05:29 +0200

    Create serializer and views for the products
    Serializer is where we
    get the information
    from the database and
    turn it into
    JSON
    so that we can
    use it in the
    frontend
    
    Add 2 pictures

--- djackets_django/product/models.py
+++ djackets_django/product/models.py
@@ -38,7 +38,7 @@ class Product(models.Model):
     
     def get_image(self):
         if self.image:
-            return 'http://127.0.0' + self.image.url
+            return 'http://127.0.0.1:8000' + self.image.url
         return ''
     
     def get_thumbnail(self):
