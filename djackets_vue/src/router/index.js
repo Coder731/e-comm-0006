8514398d21846a33b1db6db9e376217bc9f171b8
Coder731 <stephenobrien1@gmail.com>
2022-07-01 02:28:00 +0200

    Enable Viewing of a product
        -Create viewset in Django
        -Create vue page for
         showing product
        -Add link to detail page

--- djackets_vue/src/router/index.js
+++ djackets_vue/src/router/index.js
@@ -1,5 +1,7 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+import HomeView from '../views/Home.vue'
+
+import Product from '../views/Product.vue'
 
 const routes = [
   {
@@ -13,7 +15,12 @@ const routes = [
     // route level code-splitting
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
+    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+  },
+  {
+    path: '/:category_slug/:product_slug/',
+    name: 'Product',
+    component: Product
   }
 ]
 
